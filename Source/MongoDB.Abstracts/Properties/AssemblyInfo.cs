50952236d0ee43ab9049b24152578cf874e01a07
Paul Welter <pwelter@loresoft.com>
2017-04-03 20:33:00 -0500

    upgrade to Microsoft.NET.Sdk projects

--- Source/MongoDB.Abstracts/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("MongoDB.Abstracts")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: Guid("38aa4587-bd6d-4c87-886b-71509ed34d99")]
